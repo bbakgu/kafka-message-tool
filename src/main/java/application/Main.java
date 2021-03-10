c89cdfd21f59dbc9617f061ff17ead068f891660
gss.jihoonk <jihoonk@smilegate.com>
2021-03-10 11:45:53 +0900

    add icon image

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,9 +1,12 @@
 package application;
 
+import javax.imageio.ImageIO;
+
 import application.logging.Logger;
 import application.root.KafkaMessageToolApplication;
 import application.root.ApplicationRoot;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -13,7 +16,14 @@ public class Main extends Application {
 
     private final ApplicationRoot app = new KafkaMessageToolApplication(this);
 
+
     public static void main(String[] args) {
+        try {
+            com.apple.eawt.Application.getApplication().setDockIconImage(ImageIO.read(Main.class.getResourceAsStream("/kafka_tool_img.png")));
+        } catch (Exception e) {
+            // Won't work on Windows or Linux.
+        }
+
         launch(args);
     }
 
@@ -22,6 +32,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             app.start(primaryStage);
+            primaryStage.getIcons().add(new Image("kafka_tool_img.png"));
         } catch (Throwable e) {
             Logger.error("Error while starting application", e);
             System.exit(ERROR_EXIT_STATUS);
